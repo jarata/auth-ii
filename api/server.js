4edbb18704d47bda9f5b1f115d361c4d70f34198
J A <jarata@gmail.com>
2019-03-06 14:11:46 -0800

    fixed package.json typo fix and test again

--- api/server.js
+++ api/server.js
@@ -11,11 +11,11 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users' usersRouter);
+// server.use('/api/auth', authRouter);
+// server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
     res.send('GET test!')
 });
 
-modules.exports = server;
+module.exports = server;
