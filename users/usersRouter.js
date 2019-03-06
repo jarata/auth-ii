e83a61333a25b917ac8d2783394a70d91ad6d776
J A <jarata@gmail.com>
2019-03-06 15:23:07 -0800

    created check role middleware and added to usersRouter

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
-router.get('/', async (req, res) => {
+const checkRole = require('../auth/checkMiddleWare');
+
+router.get('/', checkRole('student'), async (req, res) => {
     res.send('GET Users')
 });
 module.exports = router;
